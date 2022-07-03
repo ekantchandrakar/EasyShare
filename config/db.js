2209181ffe0d2ae8e260bbb0bfe0d9fc33ca0f78
Ekant Chandrakar <ekantchandrakar07@gmail.com>
2022-07-03 23:19:31 +0530

    Initial Commit

---
+++ config/db.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+function connectDB() {
+    // Database connection
+    mongoose.connect(process.env.MONGO_CONNECTION_URL, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify : true
+    });
+
+    const connection = mongoose.connection;
+    connection.once('open', () => {
+        console.log('Database connected 🥳🥳🥳🥳');
+    }).catch(err => {
+        console.log('Connection failed ☹️☹️☹️☹️');
+    });
+}
+
+module.exports = connectDB; 
